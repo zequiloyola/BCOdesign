af233007252e31867984aa00b1e0e6748fae4899
zequiloyola <diiacovoezequiel@hotmail.com>
2021-09-13 18:34:51 -0300

    updating

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let animado = document.querySelectorAll(".animado");
+
+function mostrarScroll () {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i=0; i < animado.length; i++ ) {
+        let alturaAnimado = animado[i].offsetTop;
+        if(alturaAnimado - 600 < scrollTop) {
+            animado[i].style.opacity = 1;
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll);
